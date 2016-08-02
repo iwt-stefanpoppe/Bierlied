17e674f2d6409d5a5d08ce4cfb104e0e1ba549cf
Stefan Poppe <stefan.poppe@immonet.de>
2016-08-02 16:13:06 +0200

    Vereinfachung

--- src/Bierlied.java
+++ src/Bierlied.java
@@ -10,28 +10,17 @@ public class Bierlied {
 
             if (anzahl == 1) {
                 wort = "Flasche";
-                System.out.println(anzahl + wort + " Bier an der Wand, ");
-                System.out.println(anzahl + wort + " Bier, ");
-                anzahl--;
-                System.out.println("Keine " + wort + " Bier mehr an der Wand!");
-            }
-
-            else {
+            } else {
                 System.out.println(anzahl + " " + wort + " Bier an der Wand,");
                 System.out.println(anzahl + " " + wort + " Bier, ");
                 System.out.println("Nimm eine runter, reich sie herum,");
                 anzahl--;
-                System.out.println(anzahl + " " + wort + " Bier an der Wand!");
+
+                if (anzahl > 0)
+                    System.out.println(anzahl + " " + wort + " Bier an der Wand!");
             }
 
         }
-
-        System.out.println("Keine " + wort + " Bier mehr an der Wand,");
-        System.out.println("Keine " + wort + " Bier,");
-        System.out.println("Geh in den Laden und kauf mehr,");
-        anzahl = 99;
-        wort = "Flaschen";
-        System.out.println("dann sind es wieder " + anzahl + " " + wort + " Bier an der Wand");
 
     }
 }
